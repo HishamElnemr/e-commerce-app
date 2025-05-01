0be60f5eef0d329948f18a76330442bf6dcd2a7b
Hisham Ahmed <hishamcom48@gmail.com>
2025-05-01 14:40:41 +0300

    Add splash screen implementation and routing
    - Created a new SVG asset for the plant image.
    - Added a PNG asset for the splash screen bottom.
    - Implemented the onGenerateRoute function to handle navigation to the SplashView.
    - Developed the SplashView widget with a basic Scaffold layout.

---
+++ lib/core/helper_function.dart/on_generate_route.dart
@@ -0,0 +1,16 @@
+import 'package:e_commerce_app/fearures/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(
+        builder: (_) => const SplashView(),
+      );
+   
+    default:
+      return MaterialPageRoute(
+        builder: (_) => const Scaffold(),
+      );
+  }
+}
